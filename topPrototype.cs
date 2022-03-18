d04afbedd70aa745bafd63b556792d4d6736953f
Ryan Patch <rypatch@microsoft.com>
2022-03-18 09:52:46 -0500

    working update

--- topPrototype.cs
+++ topPrototype.cs
@@ -1,6 +1,6 @@
 using System;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
+//using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 using Azure.Messaging.ServiceBus;
 using  System.Threading.Tasks;
@@ -10,26 +10,23 @@ namespace topic.Function
 {
     public class topPrototype
     {
-
+        
         [FunctionName("topPrototype")]
-        //public static void Run([ServiceBusTrigger("ttop1", "tTop1Sub1", Connection = "cupcakes_SERVICEBUS")]ServiceBusReceivedMessage mySbMsg, ILogger log)
-        public static void Run([ServiceBusTrigger("ttop1", "tTop1Sub1", Connection = "cupcakes_SERVICEBUS")]
-        string mySbMsg,
-        Int32 deliveryCount,
-        DateTime enqueuedTimeUtc,
-        string messageId,
-        //ApplicationProperties custProp,
-        ILogger log)
+         public static void Run(
+            [ServiceBusTrigger(
+                "ttop1", 
+                "tTop1Sub1", 
+                Connection = "cupcakes_SERVICEBUS")]
+                ServiceBusReceivedMessage  messageIncoming,
+                ILogger log)
         {
-            //string msgBody = mySbMsg.Body.ToString();
-            //log.LogInformation($"{Environment.NewLine}***{Environment.NewLine} Generic Operator for message : {mySbMsg.MessageId}");
-            //log.LogInformation($"C# ServiceBus topic trigger function processed message: {msgBody}");
-            
-            
-            log.LogInformation($"{Environment.NewLine}***{Environment.NewLine} Generic Operator for message : {messageId}");
-            log.LogInformation($"{Environment.NewLine}EnqueuedTimeUtc={enqueuedTimeUtc}");
-            log.LogInformation($"{Environment.NewLine}DeliveryCount={deliveryCount}");
-            log.LogInformation($"{Environment.NewLine}C# ServiceBus topic trigger function processed message: {mySbMsg}");
+            log.LogInformation($"{Environment.NewLine}***{Environment.NewLine}Generic Operator for message : {messageIncoming.MessageId}");
+            string custProp = string.Join(Environment.NewLine,messageIncoming.ApplicationProperties);
+            var frostyVar = messageIncoming.ApplicationProperties["frosted"];
+            //log.LogInformation($"{Environment.NewLine}Custom Message Properties : {messageIncoming.ApplicationProperties.ToString()}");
+            log.LogInformation($"{Environment.NewLine}C# ServiceBus topic trigger function processed message: {messageIncoming.Body}");
+            log.LogInformation($"{Environment.NewLine}Frosty Status: {frostyVar}");
+            log.LogInformation($"{Environment.NewLine}Message Properties: {custProp}");
         }
     }
 }
